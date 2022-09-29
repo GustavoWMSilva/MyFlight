c091ba01f0751fc54a5ac56da4ef0dc64a7417ad
Lorenzo More <lorenzo.duarte@edu.pucrs.br>
2022-09-29 20:16:36 -0300

    Implementando gerenciadores

--- MyFlight_base/src/pucrs/myflight/modelo/GerenciadorCias.java
+++ MyFlight_base/src/pucrs/myflight/modelo/GerenciadorCias.java
@@ -2,10 +2,46 @@ package pucrs.myflight.modelo;
 
 import java.util.ArrayList;
 
-public class GerenciadorCias {
+public class GerenciadorCias 
+{
 	private ArrayList<CiaAerea> empresas;
 	
-	public GerenciadorCias() {
+	public GerenciadorCias() 
+	{
 		empresas = new ArrayList<>();
+	}
+
+	public void adicionar (CiaAerea cia) 
+	{
+		empresas.add(cia);
+	}
+
+	//public ArrayList <CiaAerea> listarTodas() {return empresas;}
+
+
+	public CiaAerea buscarCodigo (String cod) 
+	{
+		for (CiaAerea a : empresas) 
+		{
+			if (a.getCodigo().equals(cod)) 
+			{
+				return a;
+			}
+			
+		}
+		return null;
+	}
+
+	public CiaAerea buscarNome (String nome) 
+	{
+		for (CiaAerea a : empresas) 
+		{
+			if (a.getNome().equals(nome)) 
+			{
+				return a;
+			}
+			
+		}
+		return null;
 	}
 }
--- MyFlight_base/src/pucrs/myflight/modelo/GerenciadorVoos.java
+++ MyFlight_base/src/pucrs/myflight/modelo/GerenciadorVoos.java
@@ -1,5 +1,22 @@
 package pucrs.myflight.modelo;
 
-public class GerenciadorVoos {
+import java.util.ArrayList;
 
+public class GerenciadorVoos 
+{  
+    private ArrayList <Voo> voos;
+
+    public GerenciadorVoos () 
+    {
+        voos = new ArrayList<>();
+    }
+
+    public void adicionar (Voo voo) 
+    {
+        voos.add(voo);
+    }
+
+    //public ArrayList <Voo> listarTodos() {return voos;}
+
+    //public ArrayList <Voo> buscarData (LocalDate data) {}
 }
